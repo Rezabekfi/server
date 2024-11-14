749f64f3c0f6db71ee910cfe2bbccc52c132d9b0
Rezabekfi <filip.rezabek@gmail.com>
2024-11-14 11:28:58 +0100

    messages udpated most likely finished

--- include/message.h
+++ include/message.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <optional>
+#include <unordered_map>
 
 enum MessageType {
     GAME_STARTED,
@@ -29,6 +30,7 @@ public:
     // Setters
     void set_type(MessageType type);
     void set_value(const std::string& key, const std::string& value);
+    void set_lobby_id(const std::string& lobby_id);
     
     // Getters
     MessageType get_type() const;
@@ -36,13 +38,16 @@ public:
     
     // Conversion
     std::string to_json() const;
+    MessageType string_to_message_type(const std::string& typeStr);
+    std::string message_type_to_string(MessageType type);
 
     // Validation
     bool validate();
 
-    void set_lobby_id(const std::string& lobby_id);
-    void setup_message_type();
-    
+    std::string message_type_to_string(MessageType type);
+    MessageType string_to_message_type(const std::string& typeStr);
+    void setup_message_type(const std::string& type_str);
+
     // Static factory methods
     static Message create_game_started();
     static Message create_welcome(const std::string& welcome_text);
--- src/message.cpp
+++ src/message.cpp
@@ -21,7 +21,6 @@ Message::Message(const std::string& json_string) {
         
         pos = value_end + 1;
     }
-
 }
 
 bool Message::validate() {
@@ -80,4 +79,41 @@ Message Message::create_waiting(const std::string& wait_text) {
     msg.set_type(MessageType::WAITING);
     msg.set_value("message", wait_text);
     return msg;
-} 
+}
+
+const std::map<MessageType, std::string> type_to_string_map = {
+    { GAME_STARTED, "game_started" },
+    { WELCOME, "welcome" },
+    { WAITING, "waiting" },
+    { GAME_ENDED, "game_ended" },
+    { MOVE, "move" },
+    { MOVE_RESULT, "move_result" },
+    { ERROR, "error" },
+    { WRONG_MESSAGE, "wrong_message" }
+};
+
+const std::map<std::string, MessageType> string_to_type_map = {
+    { "game_started", GAME_STARTED },
+    { "welcome", WELCOME },
+    { "waiting", WAITING },
+    { "game_ended", GAME_ENDED },
+    { "move", MOVE },
+    { "move_result", MOVE_RESULT },
+    { "error", ERROR },
+    { "wrong_message", WRONG_MESSAGE }
+};
+
+// Function implementations
+std::string Message::message_type_to_string(MessageType type) {
+    auto it = type_to_string_map.find(type);
+    return (it != type_to_string_map.end()) ? it->second : "unknown";
+}
+
+MessageType Message::string_to_message_type(const std::string& typeStr) {
+    auto it = string_to_type_map.find(typeStr);
+    return (it != string_to_type_map.end()) ? it->second : WRONG_MESSAGE;
+}
+
+void Message::setup_message_type(const std::string& type_str) {
+    type = string_to_message_type(type_str);
+}
