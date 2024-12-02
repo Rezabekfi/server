51d2d8d00cb2a1df9039bfbbee8ea6832d4b442f
Rezabekfi <filip.rezabek@gmail.com>
2024-12-02 19:39:02 +0100

    port from settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include "quoridor_server.h"
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <any>
 
 int main() {
     try {
-        std::cout << "Starting server... fr fr fr" << std::endl;
+        std::ifstream settings_file("../connection_settings.txt");
+        if (!settings_file.is_open()) {
+            throw std::runtime_error("Could not open connection settings file.");
+        }
+
+        nlohmann::json settings;
+        settings_file >> settings;
+        int port = settings["port"];
+
+        std::cout << "Starting server on port " << port << "..." << std::endl;
         QuoridorServer server;
-        server.start(5002);
+        server.start(port);
     } catch (const std::exception& e) {
         std::cerr << "Server error: " << e.what() << std::endl;
         return 1;
     }
     return 0;
-} 
+}
